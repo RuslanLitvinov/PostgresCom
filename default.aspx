a24792525d5cc99fa734a8a157de47d8c5d2494b
Ruslan Litvinov <RuslanLitvinov70@gmail.com>
2019-08-22 16:12:21 +0500

    Пробное взаимодействие с Postgress - подключение пользователя к базе

---
+++ default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="PostgresCom.first" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 140px">
+            <asp:Label ID="Label1" runat="server" Text="Имя пользователя:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="LoginTxt" runat="server"></asp:TextBox>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label3" runat="server" Text="Пароль:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="PassTxt" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ConnectionBtn" runat="server" OnClick="ConnectionBtn_Click" Text="Подключение" />
+            <br />
+            <br />
+            Статус подключени: <asp:Label ID="ConnectionStatus" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Common;
+
+namespace PostgresCom
+{
+    public partial class first : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ConnectionBtn_Click(object sender, EventArgs e)
+        {
+            string connString = string.Format("Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                   "localhost",
+                   LoginTxt.Text,
+                   LoginTxt.Text,
+                   "5432",
+                   PassTxt.Text
+                );
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    ConnectionStatus.Text = "Подключение успешно";
+                    conn.Close();
+                }
+                catch (NpgsqlException ex)
+                {
+                    if (ex.Message.IndexOf("28P01") > -1)
+                    {
+                        ConnectionStatus.Text = "Неверное имя пользователя или пароль.";
+                    }
+                    else
+                    {
+                        ConnectionStatus.Text = "Ошибка подключения: "+ex.ErrorCode.ToString() + " сообщение: " + ex.Message;
+                    }
+                }
+            }
+        }
+    }
+}
